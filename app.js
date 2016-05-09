935048942c4046c08cfee230b0f81b8bffec3f58
Carlos Domingues dos Santos <carlosceds034@gmail.com>
2016-05-09 20:54:34 -0300

    [Carlos] Challenge: Implemented function to search instagram users, implemented function to show recent posts, implemented function to search tags. Username is a link to user profile's page, and hashtag is a link to item posted.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 (function(){
-	var app = angular.module('myApp', []);
-	app.controller('hashtagController', function($http) {
-    	var hashtag = this;
+    var app = angular.module('myApp', []);
+	app.controller('hashtagController', ['$http', function($http) {
+    	var hashtag = this;       
 	    hashtag.data = [];
-	    $http.get('https://api.instagram.com/v1/users/self/?access_token=3209101072.e25468f.192b2d5101b6417a8d86de56a8ae9e64').success(function(data){
-	        hashtag.data = data;
+	    $http({
+            method: 'JSONP',
+            url:'https://api.instagram.com/v1/users/self/?access_token=3209101072.e25468f.192b2d5101b6417a8d86de56a8ae9e64'}).success(function(data){
+	           console.log(data);
 	    });
-	});
+	}]);
 
 	app.controller('TabController', function($scope){
     	$scope.tab = 1;
